8b47301329d75bbfb17c1a3cd6254fcb5d99f611
qqibrow <qqibrow@gmail.com>
2014-11-17 23:01:52 -0800

    Add the pass-in meme words feature.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['mp4','txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['ALLOWED_EXTENSIONS'] = set(['mp4'])
 
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
@@ -34,6 +34,7 @@ def index():
 def upload():
     # Get the name of the uploaded file
     file = request.files['file']
+    words = request.args.get('words')
     # Check if the file is one of the allowed types/extensions
     if file and allowed_file(file.filename):
         # Make the filename safe, remove unsupported chars
@@ -41,17 +42,20 @@ def upload():
         # Move the file form the temporal folder to
         # the upload folder we setup
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # Redirect the user to the uploaded_file route, which
-        # will basicaly show on the browser the uploaded file
-        #return "receved video " + filename
+        
+        # Create gif clip
         clip = (VideoFileClip('./uploads/' + filename, audio=False)
-                .subclip((0,00.05),(0,01.00)).resize(0.3))
-        text = (TextClip("Working hard to DEMO!", fontsize=30, color='black',font='Amiri-Bold')
-                .set_pos((0,0)).set_duration(clip.duration))
-        composition = CompositeVideoClip([clip, text])
+                    .subclip((0,00.05),(0,01.00)).resize(0.3))
+        finalClip = None
+        if words is None:            
+            finalClip = clip
+        else:
+            text = (TextClip(words, fontsize=30, color='black')
+                    .set_pos((0,0)).set_duration(clip.duration))
+            finalClip = CompositeVideoClip([clip, text])            
         gif_file_name = filename.split('.',1)[0] + '.gif'
         print gif_file_name
-        composition.write_gif("./uploads/" + gif_file_name, fps=10, fuzz=2)
+        finalClip.write_gif("./uploads/" + gif_file_name, fps=10, fuzz=2)
         return redirect(url_for('uploaded_file',filename=gif_file_name))
 
 # This route is expecting a parameter containing the name
